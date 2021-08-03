3b3de1d152c216f2c29fea9ccfb579a61b9e7279
Jui1710 <87124714+Jui1710@users.noreply.github.com>
2021-08-03 15:47:24 +0530

    make it multiple circular shifts

--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -4,6 +4,24 @@ const rotateString = (str, n, direction) => {
   //
   // input str = random ,1, right
   // output mrando
+  if ((typeof (str) === 'string') && (typeof (n) === 'number') && (n >= 0)) {
+
+    if (direction === 'left') {
+      return str.substring(n) + str.substring(0, n);
+    }
+    else if (direction === 'right') {
+      let reversestring = str.split("").reverse().join("");
+
+      return reversestring.substring(0, n) + str.substring(0, str.length - n);
+    } else {
+      throw new Error("Invalid Direction");
+    }
+
+
+  }
+  else {
+    throw new Error("Invalid Input");
+  }
 };
 
 module.exports = rotateString;
