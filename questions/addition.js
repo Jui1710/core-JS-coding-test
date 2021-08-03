52941bf1862df8a42e4caba24e92e815dfd3e7a9
Jui1710 <87124714+Jui1710@users.noreply.github.com>
2021-08-03 11:16:19 +0530

    Add files via upload

---
+++ questions/addition.js
@@ -0,0 +1,11 @@
+/* Addition function takes a list of numbers and returns the sum
+1. Take anything between 1 to n arguments and return sum 
+2. Throw and error with message "Invalid Input" when no arguments are passed. 
+3. Detect non numeric input and throw an error with message "Invalid Input"
+*/
+
+const addition = (...numbers) => {
+  // all your code
+};
+
+module.exports = addition;
