b65fc929042f873a315b047b8c970626a36e8a03
Jui1710 <87124714+Jui1710@users.noreply.github.com>
2021-08-03 12:17:27 +0530

    convertTime done

--- questions/convertTime.js
+++ questions/convertTime.js
@@ -6,6 +6,23 @@
  The value corresponding to each key should be the conversion of seconds into correponding 
  time 
  */
-const convertTime = (seconds) => {};
+const convertTime = (seconds) => {
+    if (typeof (seconds) === 'number') {
+        const time = {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        }
+        time.hours = Math.floor(seconds / 3600);
+        seconds = seconds % 3600;
+        time.minutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        time.seconds = seconds;
+
+        return time;
+    } else {
+        throw new Error('Invalid Input');
+    }
+};
 
 module.exports = convertTime;
